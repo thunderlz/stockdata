f948d0ef80319212566ba69214844d72a445363f
雷震 <leizhen@Leizs-Mac-Pro.local>
2021-06-27 09:27:59 +0800

    加入了基金的数据获取

--- tushareget.py
+++ tushareget.py
@@ -2,7 +2,8 @@ import datamanage
 import datetime
 
 if __name__=='__main__':
-    dm = datamanage.datamanage((datetime.datetime.today()-datetime.timedelta(8000)).strftime('%Y%m%d'))
+    dm = datamanage.datamanage(startdate=(datetime.datetime.today()-datetime.timedelta(5)).strftime('%Y%m%d'))
     dm.getstocksdaily()
     dm.getindexdaily()
     dm.getadjfactor()
+    dm.getfundsdaily()
