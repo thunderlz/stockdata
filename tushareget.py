b9bb14f87142c32d087cd6c149150bce5a8680e7
leizhen <thunderlz@163.com>
2021-11-08 09:39:29 +0800

    把basic信息的获取顺序放在了前面

--- tushareget.py
+++ tushareget.py
@@ -4,7 +4,7 @@ import datetime
 if __name__=='__main__':
     dm = datamanage.datamanage(startdate=(datetime.datetime.today()-datetime.timedelta(5)).strftime('%Y%m%d'))
     dm.getstocksdaily()
+    dm.getbasicdaily()
     dm.getindexdaily()
     dm.getadjfactor()
     dm.getfundsdaily()
-    dm.getbasicdaily()
